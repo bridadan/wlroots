2f00d39ce7d52d671364cabe7ea1e020b74a0c43
Brian Daniels <brianddaniels@gmail.com>
2020-04-19 22:41:14 -0400

    Remove popup

--- examples/layer-shell.c
+++ examples/layer-shell.c
@@ -35,13 +35,6 @@ struct wlr_egl_surface *egl_surface;
 struct wl_callback *frame_callback;
 
 static uint32_t output = UINT32_MAX;
-struct xdg_popup *popup;
-struct wl_surface *popup_wl_surface;
-struct wl_egl_window *popup_egl_window;
-static uint32_t popup_width = 256, popup_height = 256;
-struct wlr_egl_surface *popup_egl_surface;
-struct wl_callback *popup_frame_callback;
-float popup_alpha = 1.0, popup_red = 0.5f;
 
 static uint32_t layer = ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND;
 static uint32_t anchor = 0;
@@ -56,7 +49,6 @@ static int cur_x = -1, cur_y = -1;
 static int buttons = 0;
 
 struct wl_cursor_image *cursor_image;
-struct wl_cursor_image *popup_cursor_image;
 struct wl_surface *cursor_surface, *input_surface;
 
 static struct {
@@ -66,7 +58,6 @@ static struct {
 } demo;
 
 static void draw(void);
-static void draw_popup(void);
 
 static void surface_frame_callback(
 		void *data, struct wl_callback *cb, uint32_t time) {
@@ -77,19 +68,6 @@ static void surface_frame_callback(
 
 static struct wl_callback_listener frame_listener = {
 	.done = surface_frame_callback
-};
-
-static void popup_surface_frame_callback(
-		void *data, struct wl_callback *cb, uint32_t time) {
-	wl_callback_destroy(cb);
-	popup_frame_callback = NULL;
-	if (popup) {
-		draw_popup();
-	}
-}
-
-static struct wl_callback_listener popup_frame_listener = {
-	.done = popup_surface_frame_callback
 };
 
 static void draw(void) {
@@ -147,105 +125,6 @@ static void draw(void) {
 	demo.last_frame = ts;
 }
 
-static void draw_popup(void) {
-	static float alpha_mod = -0.01;
-
-	eglMakeCurrent(egl.display, popup_egl_surface, popup_egl_surface, egl.context);
-	glViewport(0, 0, popup_width, popup_height);
-	glClearColor(popup_red, 0.5f, 0.5f, popup_alpha);
-	popup_alpha += alpha_mod;
-	if (popup_alpha < 0.01 || popup_alpha >= 1.0f) {
-		alpha_mod *= -1.0;
-	}
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	popup_frame_callback = wl_surface_frame(popup_wl_surface);
-	assert(popup_frame_callback);
-	wl_callback_add_listener(popup_frame_callback, &popup_frame_listener, NULL);
-	eglSwapBuffers(egl.display, popup_egl_surface);
-	wl_surface_commit(popup_wl_surface);
-}
-
-static void xdg_surface_handle_configure(void *data,
-		struct xdg_surface *xdg_surface, uint32_t serial) {
-	xdg_surface_ack_configure(xdg_surface, serial);
-}
-
-static const struct xdg_surface_listener xdg_surface_listener = {
-	.configure = xdg_surface_handle_configure,
-};
-
-static void xdg_popup_configure(void *data, struct xdg_popup *xdg_popup,
-		int32_t x, int32_t y, int32_t width, int32_t height) {
-	wlr_log(WLR_DEBUG, "Popup configured %dx%d@%d,%d",
-			width, height, x, y);
-	popup_width = width;
-	popup_height = height;
-	if (popup_egl_window) {
-		wl_egl_window_resize(popup_egl_window, width, height, 0, 0);
-	}
-}
-
-static void popup_destroy(void) {
-	wlr_egl_destroy_surface(&egl, popup_egl_surface);
-	wl_egl_window_destroy(popup_egl_window);
-	xdg_popup_destroy(popup);
-	wl_surface_destroy(popup_wl_surface);
-	popup_wl_surface = NULL;
-	popup = NULL;
-	popup_egl_window = NULL;
-}
-
-static void xdg_popup_done(void *data, struct xdg_popup *xdg_popup) {
-	wlr_log(WLR_DEBUG, "Popup done");
-	popup_destroy();
-}
-
-static const struct xdg_popup_listener xdg_popup_listener = {
-	.configure = xdg_popup_configure,
-	.popup_done = xdg_popup_done,
-};
-
-static void create_popup(uint32_t serial) {
-	if (popup) {
-		return;
-	}
-	struct wl_surface *surface = wl_compositor_create_surface(compositor);
-	assert(xdg_wm_base && surface);
-	struct xdg_surface *xdg_surface =
-		xdg_wm_base_get_xdg_surface(xdg_wm_base, surface);
-	struct xdg_positioner *xdg_positioner =
-		xdg_wm_base_create_positioner(xdg_wm_base);
-	assert(xdg_surface && xdg_positioner);
-
-	xdg_positioner_set_size(xdg_positioner, popup_width, popup_height);
-	xdg_positioner_set_offset(xdg_positioner, 0, 0);
-	xdg_positioner_set_anchor_rect(xdg_positioner, cur_x, cur_y, 1, 1);
-	xdg_positioner_set_anchor(xdg_positioner, XDG_POSITIONER_ANCHOR_BOTTOM_RIGHT);
-
-	popup = xdg_surface_get_popup(xdg_surface, NULL, xdg_positioner);
-	xdg_popup_grab(popup, seat, serial);
-
-	assert(popup);
-
-	zwlr_layer_surface_v1_get_popup(layer_surface, popup);
-
-	xdg_surface_add_listener(xdg_surface, &xdg_surface_listener, NULL);
-	xdg_popup_add_listener(popup, &xdg_popup_listener, NULL);
-
-	wl_surface_commit(surface);
-	wl_display_roundtrip(display);
-
-	xdg_positioner_destroy(xdg_positioner);
-
-	popup_wl_surface = surface;
-	popup_egl_window = wl_egl_window_create(surface, popup_width, popup_height);
-	assert(popup_egl_window);
-	popup_egl_surface = wlr_egl_create_surface(&egl, popup_egl_window);
-	assert(popup_egl_surface);
-	draw_popup();
-}
-
 static void layer_surface_configure(void *data,
 		struct zwlr_layer_surface_v1 *surface,
 		uint32_t serial, uint32_t w, uint32_t h) {
@@ -275,11 +154,7 @@ static void wl_pointer_enter(void *data, struct wl_pointer *wl_pointer,
 		uint32_t serial, struct wl_surface *surface,
 		wl_fixed_t surface_x, wl_fixed_t surface_y) {
 	struct wl_cursor_image *image;
-	if (surface == popup_wl_surface) {
-		image = popup_cursor_image;
-	} else {
-		image = cursor_image;
-	}
+	image = cursor_image;
 	wl_surface_attach(cursor_surface,
 		wl_cursor_image_get_buffer(image), 0, 0);
 	wl_surface_damage(cursor_surface, 1, 0,
@@ -307,26 +182,11 @@ static void wl_pointer_button(void *data, struct wl_pointer *wl_pointer,
 	if (input_surface == wl_surface) {
 		if (state == WL_POINTER_BUTTON_STATE_PRESSED) {
 			if (button == BTN_RIGHT) {
-				if (popup_wl_surface) {
-					popup_destroy();
-				} else {
-					create_popup(serial);
-				}
 			} else {
 				buttons++;
 			}
 		} else {
-			if (button != BTN_RIGHT) {
-				buttons--;
-			}
-		}
-	} else if (input_surface == popup_wl_surface) {
-		if (state == WL_POINTER_BUTTON_STATE_PRESSED) {
-			if (button == BTN_LEFT && popup_red <= 0.9f) {
-				popup_red += 0.1;
-			} else if (button == BTN_RIGHT && popup_red >= 0.1f) {
-				popup_red -= 0.1;
-			}
+			buttons--;
 		}
 	} else {
 		assert(false && "Unknown surface");
@@ -605,7 +465,6 @@ int main(int argc, char **argv) {
 		cursor = wl_cursor_theme_get_cursor(cursor_theme, "left_ptr");
 	}
 	assert(cursor);
-	popup_cursor_image = cursor->images[0];
 
 	cursor_surface = wl_compositor_create_surface(compositor);
 	assert(cursor_surface);
